ef2c5ed1ebc3dca332bf7d41a92bd3ec7d3e86bd
Sofia Sampara <sampara.pn@ucu.edu.ua>
2024-09-26 09:38:45 +0300

    trying to run build tests

--- src/main/java/lotr/CharacterFactory.java
+++ src/main/java/lotr/CharacterFactory.java
@@ -3,6 +3,7 @@ package lotr;
 class CharacterFactory {
     public Character createCharacter() {
         //method to create a random character
+        //added another line
         return null;
     }
 }
